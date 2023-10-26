549d75832191a162efe70aa824ce256b4067f059
Ундрам Бат-Эрдэнэ <23LP3261@G301-CC0171.local>
2023-10-26 09:49:56 +0800

    suulinh

---
+++ time.js
@@ -0,0 +1,42 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slide = document.querySelector(".slide");
+
+let index = 1;
+
+left.addEventListener("click", () => {
+    if (index === 0) return;
+    index--;
+    slide.style.transition = "500ms";
+    slide.style.transform = `translateX(${index * -20}%)`
+});
+
+right.addEventListener("click", () => {
+    if (index === 4) return;
+    index++;
+    slide.style.transition = "500ms";
+    slide.style.transform = `translateX(${index * -20}%)`
+    clearInterval(id)
+});
+
+slide.addEventListener("transitionend", () => {
+    if (index === 4) {
+        index = 1;
+        slide.style.transition = "none";
+        slide.style.transform = `translateX(${index * -20}%)`
+    }
+});
+
+slide.addEventListener("transitionstart", () => {
+    if (index === 0) {
+        index = 3;
+        slide.style.transition = "none";
+        slide.style.transform = `translateX(${index * -20}%)`
+    }
+});
+
+const id = setInterval(() => {
+    index++;
+    slide.style.transition = "500ms";
+    slide.style.transform = `translateX(${index * -20}%)`
+}, 2000)
